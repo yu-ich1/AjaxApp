bece52bb79bee41d020c375a86ff779a8c92e198
yu-ich1 <68319482+yu-ich1@users.noreply.github.com>
2020-07-29 14:36:14 +0900

    既読機能のサーバーサイド

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,19 @@ class PostsController < ApplicationController
 
     def create
       Post.create(content: params[:content])
-      redirect_to action: :index
-
+      redirect_to action: :inde
+    end
+    def checked
+      post = Post.find(params[:id])
+      if post.checked then
+        post.update(checked: false)
+      else
+        post.update(checked: true)
+      end
+  
+      item = Post.find(params[:id])
+      render json: { post: item }
     end
   end
+
 
