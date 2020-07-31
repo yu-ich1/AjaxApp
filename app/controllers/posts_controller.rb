40fc7fe99f4861a727cc6e88f98fc4a48cc8468c
yu-ich1 <68319482+yu-ich1@users.noreply.github.com>
2020-07-31 14:59:21 +0900

    Basic認証

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :basic_auth
 
   def index
     @posts = Post.all.order(id: "DESC")
@@ -22,4 +23,11 @@ class PostsController < ApplicationController
     render json: { post: item }
   end
 
+private
+
+def basic_auth
+  authenticate_or_request_with_http_basic do |username, password|
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+  end
+end
 end
